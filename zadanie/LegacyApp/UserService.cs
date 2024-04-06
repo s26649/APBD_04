3b7373c89e06cc007ea050750394d1455f40fcaa
s26649 <s26649@pjwstk.edu.pl>
2024-04-06 21:48:06 +0200

    poprawki

--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -7,56 +7,23 @@ namespace LegacyApp
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
             if (!ValidateUserData(firstName, lastName, email, dateOfBirth)) return false;
+            
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
-            var user = CreateUser(firstName, lastName, email, dateOfBirth, client);
             
-            if (client.Type == "VeryImportantClient")
+            var user = new User
             {
-                user.HasCreditLimit = false;
-            }
-            else if (client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = email,
+                DateOfBirth = dateOfBirth,
+                Client = client
+            };
 
-            if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
-                return false;
-            }
+            if (!SetCreditLimitAndCheck(user, client)) return false;
 
             UserDataAccess.AddUser(user);
             return true;
-        }
-        
-        // oddzielenie utworzenia uzytkownika do osobnej metody
-        private User CreateUser(string firstName, string lastName, string email, DateTime dateOfBirth, Client client)
-        {
-            var user = new User
-            {
-                Client = client,
-                DateOfBirth = dateOfBirth,
-                EmailAddress = email,
-                FirstName = firstName,
-                LastName = lastName,
-                HasCreditLimit = true
-            };
-
-            return user;
         }
         
         // oddzielenie logiki walidacji do osobnej metody, poprawka czytelności (Single Responsibility Principle)
@@ -76,6 +43,32 @@ namespace LegacyApp
             if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) 
                 age--;
             return age;
+        }
+        
+        // oddzielenie logiki ustawiania limitu kredytowego i sprawdzania do osobnej metody, poprawka czytelności (Single Responsibility Principle)
+        private bool SetCreditLimitAndCheck(User user, Client client)
+        {
+            using (var userCreditService = new UserCreditService())
+            {
+                switch (client.Type)
+                {
+                    case "VeryImportantClient":
+                        user.HasCreditLimit = false;
+                        break;
+                    case "ImportantClient":
+                        var importantClientCreditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth) * 2;
+                        user.CreditLimit = importantClientCreditLimit;
+                        user.HasCreditLimit = true;
+                        break;
+                    default:
+                        var normalClientCreditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                        user.CreditLimit = normalClientCreditLimit;
+                        user.HasCreditLimit = true;
+                        break;
+                }
+            }
+
+            return !(user.HasCreditLimit && user.CreditLimit < 500);
         }
     }
 }
